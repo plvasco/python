9e67bb02ef8ec7a8e5516d4e03fa931ef3a6b2f0
plvasco <pietto@gmx.com>
2019-01-27 16:17:14 -0600

    2nd commit

---
+++ dict.py
@@ -0,0 +1,10 @@
+monthConversions = {
+    "Jan": "Janurary",
+    "Feb": "February",
+    "Mar": "March"
+}
+# print(monthConversions["Mar"])
+#print(monthConversions.get("Jan"))
+
+print(monthConversions.get("ert", "Not a valid key"))
+
---
+++ forloop.py
@@ -0,0 +1,6 @@
+
+friends = ["jim", "karen", "kevin"]
+#len(friends)
+for index in range(5):
+
+    print(friends[index])
